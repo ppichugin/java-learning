0ce9952d569e084635b6fad7494c30abff065f07
Petr Pichugin <petr.pichugin@gmail.com>
2022-05-31 11:59:28 +0500

    test with CompletableFuture.runAsync & myExecutor

--- src/kz/pichugin/module/n1_4_concurrency/Foo.java
+++ src/kz/pichugin/module/n1_4_concurrency/Foo.java
@@ -6,34 +6,46 @@ public class Foo {
     static volatile int count = 1;
     static Semaphore semCon = new Semaphore(1);
 
-    public void first(Runnable r) throws InterruptedException {
+    public void first(Runnable r) {
         while (count != 1) {
             Thread.onSpinWait();
         }
-        semCon.acquire();
-        r.run();
-        count = 2;
-        semCon.release();
+        try {
+            semCon.acquire();
+            r.run();
+            count = 2;
+            semCon.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void second(Runnable r) throws InterruptedException {
+    public void second(Runnable r) {
         while (count != 2) {
             Thread.onSpinWait();
         }
-        semCon.acquire();
-        r.run();
-        count = 3;
-        semCon.release();
+        try {
+            semCon.acquire();
+            r.run();
+            count = 3;
+            semCon.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void third(Runnable r) throws InterruptedException {
+    public void third(Runnable r) {
         while (count != 3) {
             Thread.onSpinWait();
         }
-        semCon.acquire();
-        r.run();
-        count = 1;
-        semCon.release();
+        try {
+            semCon.acquire();
+            r.run();
+            count = 1;
+            semCon.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     protected synchronized void print(String msg) {
--- src/kz/pichugin/module/n1_4_concurrency/FooMain.java
+++ src/kz/pichugin/module/n1_4_concurrency/FooMain.java
@@ -1,37 +1,27 @@
 package kz.pichugin.module.n1_4_concurrency;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class FooMain {
     public static void main(String[] args) {
         Foo foo = new Foo();
-
         Runnable printFirst = () -> foo.print("first");
         Runnable printSecond = () -> foo.print("second");
         Runnable printThird = () -> foo.print("third");
 
-        Thread threadA = new Thread(() -> {
-            try {
-                foo.first(printFirst);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }, "Thread A");
-        Thread threadB = new Thread(() -> {
-            try {
-                foo.second(printSecond);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }, "Thread B");
-        Thread threadC = new Thread(() -> {
-            try {
-                foo.third(printThird);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }, "Thread C");
+//        Thread threadA = new Thread(() -> foo.first(printFirst), "Thread A");
+//        Thread threadB = new Thread(() -> foo.second(printSecond), "Thread B");
+//        Thread threadC = new Thread(() -> foo.third(printThird), "Thread C");
+//        threadA.start();
+//        threadC.start();
+//        threadB.start();
 
-        threadA.start();
-        threadC.start();
-        threadB.start();
+        Executor myExecutor = Executors.newFixedThreadPool(3);
+        CompletableFuture<Void> first = CompletableFuture.runAsync(printFirst, myExecutor);
+        CompletableFuture<Void> second = CompletableFuture.runAsync(printSecond, myExecutor);
+        CompletableFuture<Void> third = CompletableFuture.runAsync(printThird, myExecutor);
+        CompletableFuture.allOf(first, second, third);
     }
 }
